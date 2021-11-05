d98adda0949d0cafb577f397a803e037a98f48e0
Semih Erguven <serguven@my.bcit.ca>
2021-11-05 14:57:11 -0700

    firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,12 @@
+// App's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDiIZqkXxy6ZwLfdFQ7NHXjZoMwfHEc-Yo",
+    authDomain: "excelsior-6f424.firebaseapp.com",
+    projectId: "excelsior-6f424",
+    storageBucket: "excelsior-6f424.appspot.com",
+    messagingSenderId: "993452857406",
+    appId: "1:993452857406:web:4261421e20319ab928d96f"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
